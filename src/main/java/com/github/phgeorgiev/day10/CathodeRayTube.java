6b53c2cebf2bf5c96f2d5b378dfe6f703f7e1ce7
Philip Georgiev <p.georgiev359@gmail.com>
2022-12-19 22:27:13 +0200

    Solve day 10 part 2

--- src/main/java/com/github/phgeorgiev/day10/CathodeRayTube.java
+++ src/main/java/com/github/phgeorgiev/day10/CathodeRayTube.java
@@ -10,6 +10,7 @@ public class CathodeRayTube {
   private final Deque<AddInstruction> instructionValues = new LinkedList<>();
   private int register = 1;
   private int signalSum = 0;
+  private String crt = "";
 
   public CathodeRayTube(String instructionsSet) {
     this.instructionsSet = new LinkedList<>(Arrays.stream(instructionsSet.split("\n")).toList());
@@ -19,9 +20,7 @@ public class CathodeRayTube {
     for (int i = 1; i <= 220; i++) {
       readNextInstruction(i);
 
-      if (!instructionValues.isEmpty() && instructionValues.getFirst().atInstruction() == i) {
-        register += instructionValues.pollFirst().amount();
-      }
+      calculateRegister(i);
 
       if (i % 40 == 20) {
         signalSum += register * i;
@@ -29,6 +28,27 @@ public class CathodeRayTube {
     }
 
     return signalSum;
+  }
+
+  public String getCrtOutput() {
+    for (int i = 0; i < 240; i++) {
+      readNextInstruction(i);
+
+      calculateRegister(i);
+
+      int crtPosition = i % 40;
+      if (crtPosition >= register - 1 && crtPosition <= register + 1) {
+        crt = crt.concat("#");
+      } else {
+        crt = crt.concat(".");
+      }
+
+      if (crtPosition == 39) {
+        crt = crt.concat("\n");
+      }
+    }
+
+    return crt;
   }
 
   private void readNextInstruction(int i) {
@@ -40,9 +60,14 @@ public class CathodeRayTube {
     if (instruction != null) {
       if (instruction.contains("addx")) {
         instructionValues.add(
-            new AddInstruction(Integer.parseInt(instruction.split(" ")[1]), i + 2)
-        );
+            new AddInstruction(Integer.parseInt(instruction.split(" ")[1]), i + 2));
       }
+    }
+  }
+
+  private void calculateRegister(int i) {
+    if (!instructionValues.isEmpty() && instructionValues.getFirst().atInstruction() == i) {
+      register += instructionValues.pollFirst().amount();
     }
   }
 }
